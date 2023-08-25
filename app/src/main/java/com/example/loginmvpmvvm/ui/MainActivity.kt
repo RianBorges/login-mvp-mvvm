0fca0bd47e4921817e3c8cca82dcc17514c50ea0
rianborges <rianvfborges@gmail.com>
2023-08-25 11:27:00 -0300

    Apaguei imports e linhas de código desnecessárias e também mudei o destino na nav_app

--- app/src/main/java/com/example/loginmvpmvvm/ui/MainActivity.kt
+++ app/src/main/java/com/example/loginmvpmvvm/ui/MainActivity.kt
@@ -2,18 +2,11 @@ package com.example.loginmvpmvvm.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.loginmvpmvvm.R
 import com.example.loginmvpmvvm.databinding.ActivityMainBinding
-import com.example.loginmvpmvvm.ui.splash.SplashFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    var mail  = ""
-    var pass = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
