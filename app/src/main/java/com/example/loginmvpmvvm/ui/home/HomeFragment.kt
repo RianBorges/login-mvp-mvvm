6cb9f063cfba82cf2ad92ff67d5c320d4eb85abf
rianborges <rianvfborges@gmail.com>
2023-09-01 11:50:30 -0300

    Fim Da Implementação do Navigation Comp

--- app/src/main/java/com/example/loginmvpmvvm/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/loginmvpmvvm/ui/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import com.example.loginmvpmvvm.R
 import com.example.loginmvpmvvm.databinding.FragmentHomeBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -17,7 +18,7 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activity?.intent?.getStringExtra("id")?.let {
+        activity?.intent?.getStringExtra(ID)?.let {
             viewModel.getUser(it)
         }
     }
@@ -36,16 +37,16 @@ class HomeFragment : Fragment() {
         }
         viewModel.userData.observe(viewLifecycleOwner){
             it?.let { user ->
-                binding.creatorId.text = "Identificador: " + user.creatorId
-                binding.createdTime.text = "Criado Em: " + user.createdTime
-                binding.modefiedTime.text = "Modificado Em: " + user.modifiedTime
-                binding.birthdate.text = "Data De Nascimento: " + user.birthdate
-                binding.phone.text = "Telefone: " + user.phone
-                binding.name.text = "Nome: " + user.name
-                binding.type.text = "Função: " + user.type
-                binding.rowId.text = "Identificador: " + user.rowId
-                binding.email.text = "Email: " + user.email
-                binding.status.text = "Status: " + user.status
+                binding.creatorId.text = getString(R.string.txt_identificator, user.creatorId)
+                binding.createdTime.text = getString(R.string.txt_created_time, user.createdTime)
+                binding.modefiedTime.text = getString(R.string.txt_modified_time, user.modifiedTime)
+                binding.birthdate.text = getString(R.string.txt_birthdate, user.birthdate)
+                binding.phone.text = getString(R.string.txt_phone, user.phone)
+                binding.name.text = getString(R.string.txt_name, user.name)
+                binding.type.text = getString(R.string.txt_type, user.name)
+                binding.rowId.text = getString(R.string.txt_row_identificator, user.rowId)
+                binding.email.text = getString(R.string.txt_email, user.email)
+                binding.status.text = getString(R.string.txt_status, user.status)
             }
         }
     }
@@ -53,12 +54,12 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
     companion object {
+        private const val ID = "id"
         fun newInstance(
             id: String = ""
-        ): HomeFragment = HomeFragment().apply {
-            arguments = bundleOf("id" to id)
+        ):HomeFragment = HomeFragment().apply {
+            arguments = bundleOf(ID to id)
         }
     }
 }
