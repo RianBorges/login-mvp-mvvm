056bf21947c577c65fc5d7df35c1758c5474b954
rianborges <rianvfborges@gmail.com>
2023-09-01 11:55:25 -0300

    Removendo Imports Desnecessários

--- app/src/main/java/com/example/loginmvpmvvm/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/loginmvpmvvm/adapters/ViewPagerAdapter.kt
@@ -4,8 +4,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.loginmvpmvvm.ui.access.signup.SignUpFragment
-import com.example.loginmvpmvvm.ui.access.login.LoginFragment
 
 class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle): FragmentStateAdapter(fragmentManager, lifecycle) {
 
